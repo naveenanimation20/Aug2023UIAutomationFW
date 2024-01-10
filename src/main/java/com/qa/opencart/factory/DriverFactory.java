edf665dae581c901d5eb05d295b4254d10d502a2
Naveen AutomationLabs <naveenanimation20@gmail.com>
2024-01-10 08:10:16 +0530

    updated aws ip

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -173,6 +173,7 @@ public class DriverFactory {
 			e.printStackTrace();
 		}
 		return prop;
+		
 
 	}
 
--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -47,6 +47,18 @@ public class OptionsManager {
 		fo = new FirefoxOptions();
 		if(Boolean.parseBoolean(prop.getProperty("headless").trim())) fo.addArguments("--headless");
 		if(Boolean.parseBoolean(prop.getProperty("incognito").trim())) fo.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			fo.setCapability("browserName", "firefox");
+			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+			
+			Map<String, Object> selenoidOptions = new HashMap<>();
+			selenoidOptions.put("screenResolution", "1280x1024x24");
+			selenoidOptions.put("enableVNC", true);
+			selenoidOptions.put("name", prop.getProperty("testname"));
+			fo.setCapability("selenoid:options", selenoidOptions);
+
+		}
 		return fo;
 	}
 	
